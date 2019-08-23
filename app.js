92862311be58641726342279cbe9dd0305594959
StephoSlash <54440629+StephoSlash@users.noreply.github.com>
2019-08-23 13:57:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const app = express();
 
 // TODO: Enter the path to your service account json file
 // Need help with this step go here: https://firebase.google.com/docs/admin/setup
-// const serviceAccount = require("./REPLACE_WITH_SERVICE_ACCOUNT.json");
+// const serviceAccount = require("./madden-2019-firebase-adminsdk-7f73f-e7cdbe1e98.json");
 
 // TODO: Enter your database url from firebase
 // admin.initializeApp({
 //   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: "https://<DATABASE_NAME>.firebaseio.com/"
+//   databaseURL: "https://madden-2019.firebaseio.com/"
 // });
 
 
